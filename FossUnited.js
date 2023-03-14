c98b504270a9dcbb6b3244388972d7446aa85290
Lily-Sneha <lakralily3@gmail.com>
2023-03-14 16:35:23 +0530

    committed first

---
+++ FossUnited.js
@@ -0,0 +1,21 @@
+// function myFunction() {
+//     var x = document.getElementById("navigation");
+//     if (x.style.display == "none") {
+//         x.style.display = "flex";
+//     } else {
+//         x.style.display = "none";
+//     }
+
+// }
+
+
+
+function myFunction() {
+    var x = document.getElementById("navi");
+    if (x.style.display == "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+
+}
