ca82c5a98ecb49c6fc58dfb4a7ed23e5319cd694
Lily-Sneha <lakralily3@gmail.com>
2023-04-03 16:46:39 +0530

    changed nav links

--- index.js
+++ index.js
@@ -45,3 +45,21 @@ document.addEventListener("click", function (e) {
         myModal.show();
     }
 })
+
+// Gallery swiper 
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
