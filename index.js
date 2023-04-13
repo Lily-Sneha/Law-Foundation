fe2af04e225c7512f5c450b0917ae922ff121af3
Lily-Sneha <lakralily3@gmail.com>
2023-04-13 21:42:08 +0530

    stories of changes

--- index.js
+++ index.js
@@ -65,3 +65,25 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+
+
+
+
+// swiper
+
+var swiper = new Swiper('.swiper-container.two', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    effect: 'coverflow',
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows: false,
+    }
+});
