12c3f73a8f412f0b1ca831928e53795804a8432b
Lily-Sneha <lakralily3@gmail.com>
2023-04-14 19:12:31 +0530

    text edited

--- index.js
+++ index.js
@@ -72,18 +72,18 @@ var swiper = new Swiper(".mySwiper", {
 
 // swiper
 
-var swiper = new Swiper('.swiper-container.two', {
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    effect: 'coverflow',
-    loop: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflow: {
-        rotate: 0,
-        stretch: 100,
-        depth: 150,
-        modifier: 1.5,
-        slideShadows: false,
-    }
-});
+// var swiper = new Swiper('.swiper-container.two', {
+//     pagination: '.swiper-pagination',
+//     paginationClickable: true,
+//     effect: 'coverflow',
+//     loop: true,
+//     centeredSlides: true,
+//     slidesPerView: 'auto',
+//     coverflow: {
+//         rotate: 0,
+//         stretch: 100,
+//         depth: 150,
+//         modifier: 1.5,
+//         slideShadows: false,
+//     }
+// });
